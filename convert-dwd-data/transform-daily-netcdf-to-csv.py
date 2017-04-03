195a5b66a5deb0acee1e839f1e3e6148ec8ea72d
Michael Berg-Mohnicke <michael.berg@zalf.de>
2017-04-03 10:57:59 +0200

    dev dump

--- convert-dwd-data/transform-daily-netcdf-to-csv.py
+++ convert-dwd-data/transform-daily-netcdf-to-csv.py
@@ -34,54 +34,15 @@ print sys.path
 from netCDF4 import Dataset
 import numpy as np
 
-def create_regnie_dicts():
-    "create a nested ordered dicts which map regnie lat/lon coordinates to y/x coordinates"
-    ydelta_grad = 1.0 / 120.0
-    xdelta_grad = 1.0 / 60.0
-    regnie_lat_lon = OrderedDict()
-    for y in range(971):
-        lat = (55.0 + 10.0 * ydelta_grad) - (y - 1) * ydelta_grad
-        for x in range(611):
-            lon = (6.0 - 10.0 * xdelta_grad) + (x - 1) * xdelta_grad
-            if not lat in regnie_lat_lon:
-                regnie_lat_lon[lat] = OrderedDict()
-            regnie_lat_lon[lat][lon] = (y, x)
-    return regnie_lat_lon
-
-def convert_regnie_pixel_to_geographic_coordinates(cartesian_point_regnie): # y, x
-    " Berechnungsfunktion"
-    xdelta_grad = 1.0 /  60.0
-    ydelta_grad = 1.0 / 120.0
-    lat = (55.0 + 10.0 * ydelta_grad) - (cartesian_point_regnie[0] - 1) * ydelta_grad
-    lon = ( 6.0 - 10.0 * xdelta_grad) + (cartesian_point_regnie[1] - 1) * xdelta_grad
-    return lat, lon
-
-
-def read_daily_regnie_ascii_grid(path_to_file):
-    "read an ascii grid into a map, without the no-data values"
-    with open(path_to_file) as file_:
-        data = {}
-        row = 0
-        for line in file_:
-            if line[0] == " ":
-                continue
-            col = 0
-            for i in range(0, len(line)-1, 4):
-                val = int(line[i:i+4])
-                if val != -999:
-                    data[(row, col)] = val / 10.0
-                col += 1
-            row += 1
-        return data
-
+LOCAL_RUN = True
 
 def main():
 
     config = {
-        "path_to_data": "m:/data/climate/dwd/grids/germany/daily/",
-        "path_to_output": "m:/data/climate/dwd/csvs/germany/",
-        "start": 1,
-        "end": 1296
+        "path_to_data": "m:/data/climate/dwd/grids/germany/daily/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/grids/germany/daily/",
+        "path_to_output": "m:/data/climate/dwd/csvs/germany/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/csvs/germany/",
+        "from-y": 1,
+        "to-y": -1
     }
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
@@ -89,101 +50,82 @@ def main():
             if kkk in config:
                 config[kkk] = int(vvv)
 
-    path_to_data = config["path_to_data"]
-    path_to_output = config["path_to_output"]
-
-
-    def find_closest_value(ord_dict, val):
-        "find the closest key to value val in ordered dict ord_dict"
-        it1 = ord_dict.iterkeys()
-        it2 = ord_dict.iterkeys()
-        next(it2, None)
-
-        for (cur, nex) in itertools.izip_longest(it1, it2, fillvalue=None):
-            cur_dist = val - cur
-            nex_dist = nex - lat
-            if cur and not nex:
-                return cur
-            elif cur <= val and val <= nex:
-                return cur if cur_dist <= nex_dist else nex
-
-        return None
-
-
-    def find_closest_lat_lon(regdic, lat, lon):
-        "find the (row, col) of the closest lat/lon value in dictionary regdic"
-        closest_lat = find_closest_value(regdic, lat)
-        if closest_lat:
-            closest_lon = find_closest_value(regdic[closest_lat], lon)
-            if closest_lon:
-                return regdic[closest_lat][closest_lon]
-
-        return (None, None)
-
-
     files = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
-    for f in os.listdir(path_to_data):
+    for f in os.listdir(config["path_to_data"]):
         elem, year_month, _ = str(f).split("_")
         year = int(year_month[:4])
         month = int(year_month[4:])
-        files[year][month][elem] = path_to_data + f
-
-    regnie_lat_lon = create_regnie_dicts()
+        files[year][month][elem] = config["path_to_data"] + f
 
     for year, months in files.iteritems():
+        #print "year: ", year, "months: ",
         for month, elems in months.iteritems():
+            print "year: ", year, " month: ", month, " ys -> ",
             data = {}
             for elem, filepath in elems.iteritems():
                 data[elem] = Dataset(filepath)
 
             ref_data = data["tavg"]
             no_of_days = len(ref_data.variables["time"])
-            
-            # put all precip data of current month into dict for easy access like with netcdfs
-            regnie = {}
-            for day in range(no_of_days):
-                path_to_regniefile = path_to_data + "../daily-regnie/ra" + str(year) + "m/ra" + date(year, month, day+1).strftime("%y%m%d")
-                regnie[day] = read_daily_regnie_ascii_grid(path_to_regniefile)
 
-            for y in range(len(ref_data.dimensions["y"])):
+            start_month = time.clock()
+            cache = defaultdict(list)
+
+            for y in range(config["from-y"] - 1, len(ref_data.dimensions["y"]) if config["to-y"] < 0 else config["to-y"]):
+                #print "y: ", y, "->"
+                start_y = time.clock()
+                #print y,
                 for x in range(len(ref_data.dimensions["x"])):
+                    #print x,
 
-                    lat = ref_data.variables["lat"][y, x]
-                    lon = ref_data.variables["lon"][y, x]
-                    reg_row, reg_col = find_closest_lat_lon(regnie_lat_lon, lat, lon)
-                    reg_coord = (reg_row, reg_col) if reg_row and reg_col else None
-
-                    if not reg_coord:
-                        print "year: ", year, " month: ", " no regnie coord found for lat/lon: ", lat, "/", lon, " y/x: ", y, "/", x, " skipping"
+                    if ref_data.variables["temperature"][0, y, x] is np.ma.masked:
                         continue
 
-                    path_to_outdir = path_to_output + "row-" + str(y) + "/"
-                    if not os.path.isdir(path_to_outdir):
-                        os.makedirs(path_to_outdir)
+                    #lat = ref_data.variables["lat"][y, x]
+                    #lon = ref_data.variables["lon"][y, x]
 
-                    path_to_outfile = path_to_outdir + "col-" + str(x)
-                    if not os.path.isfile(path_to_outfile):
-                        with open(path_to_outfile, "w") as _:
-                            _.write(["iso-date", "tmin", "tavg", "tmax", "precip", "relhumid", "globrad", "windspeed"])
-                            _.write(["[]", "[°C]", "[°C]", "[°C]", "[mm]", "[%]", "[MJ m-2]", "[m s-1]"])
+                    for i in range(no_of_days):
+                        row = [
+                            date(year, month, i+1).strftime("%Y-%m-%d"),
+                            str(data["tmin"].variables["temperature"][i][y][x]),
+                            str(data["tavg"].variables["temperature"][i][y][x]),
+                            str(data["tmax"].variables["temperature"][i][y][x]),
+                            str(data["precip"].variables["precipitation"][i][y][x]),
+                            str(data["RH"].variables["humidity"][i][y][x]),
+                            str(round(data["SIS"].variables["SIS"][i][y][x] * 3600 / 1000000, 4)),
+                            str(data["FF"].variables["FF"][i][y][x])
+                        ]
+                        cache[(y,x)].append(row)
+                
+                end_y = time.clock()
+                print str(y) + "|" + str(int(end_y - start_y)) + "s ",
+                #print ""
 
-                    with open(path_to_outdir + "col-" + str(x), "a") as _:
-                        writer = csv.writer(_, delimiter=",")
-                        for i in range(no_of_days):
-                            row = [
-                                date(year, month, i+1).strftime("%Y-%m-%d"),
-                                str(data["tmin"].variables["temperature"][i][y][x]),
-                                str(data["tavg"].variables["temperature"][i][y][x]),
-                                str(data["tmax"].variables["temperature"][i][y][x]),
-                                str(regnie[i][reg_coord]) if reg_coord and reg_coord in regnie[i] else "0",
-                                str(data["RH"].variables["humidity"][i][y][x]),
-                                str(data["SIS"].variables["SIS"][i][y][x]),
-                                str(data["FF"].variables["FF"][i][y][x])
-                            ]
-                            writer.writerow(row)
-            
             for dataset in data.values():
                 dataset.close()
+            print ""
 
+            end_filled_cache = time.clock()
+            print "filling cache took ", (end_filled_cache - start_month), " seconds"
+
+            for (y, x), rows in cache.iteritems():
+                path_to_outdir = config["path_to_output"] + "row-" + str(y) + "/"
+                if not os.path.isdir(path_to_outdir):
+                    os.makedirs(path_to_outdir)
+
+                path_to_outfile = path_to_outdir + "col-" + str(x) + ".csv"
+                if not os.path.isfile(path_to_outfile):
+                    with open(path_to_outfile, "wb") as _:
+                        writer = csv.writer(_, delimiter=",")
+                        writer.writerow(["iso-date", "tmin", "tavg", "tmax", "precip", "relhumid", "globrad", "windspeed"])
+                        writer.writerow(["[]", "[°C]", "[°C]", "[°C]", "[mm]", "[%]", "[MJ m-2]", "[m s-1]"])
+
+                with open(path_to_outfile, "ab") as _:
+                    writer = csv.writer(_, delimiter=",")
+                    for row in rows:
+                        writer.writerow(row)
+
+            end_wrote_files = time.clock()
+            print "writing files took ", (end_wrote_files - end_filled_cache), " seconds"
 
 main()
--- convert-dwd-data/transform-regnie-grids-to-netcdf.py
+++ convert-dwd-data/transform-regnie-grids-to-netcdf.py
@@ -15,6 +15,9 @@
 # Landscape Systems Analysis at the ZALF.
 # Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
 
+#pylint: disable=C0103
+
+#%%
 import time
 import os
 import math
@@ -25,6 +28,7 @@ import itertools
 #import copy
 from StringIO import StringIO
 from datetime import date, datetime, timedelta
+import calendar
 from collections import defaultdict, OrderedDict
 #import types
 import sys
@@ -35,20 +39,20 @@ print sys.path
 from netCDF4 import Dataset
 import numpy as np
 
+#%%
 def create_regnie_dicts():
     "create a nested ordered dicts which map regnie lat/lon coordinates to y/x coordinates"
     ydelta_grad = 1.0 / 120.0
     xdelta_grad = 1.0 / 60.0
-    regnie_lat_lon = OrderedDict()
-    for y in range(971):
+    regnie_lat_lon = defaultdict(dict)
+    for y in range(1, 971 + 1):
         lat = (55.0 + 10.0 * ydelta_grad) - (y - 1) * ydelta_grad
-        for x in range(611):
+        for x in range(1, 611 + 1):
             lon = (6.0 - 10.0 * xdelta_grad) + (x - 1) * xdelta_grad
-            if not lat in regnie_lat_lon:
-                regnie_lat_lon[lat] = OrderedDict()
-            regnie_lat_lon[lat][lon] = (y, x)
+            regnie_lat_lon[lat][lon] = (y - 1, x - 1)
     return regnie_lat_lon
 
+#%%
 def convert_regnie_pixel_to_geographic_coordinates(cartesian_point_regnie): # y, x
     " Berechnungsfunktion"
     xdelta_grad = 1.0 /  60.0
@@ -57,100 +61,128 @@ def convert_regnie_pixel_to_geographic_coordinates(cartesian_point_regnie): # y,
     lon = ( 6.0 - 10.0 * xdelta_grad) + (cartesian_point_regnie[1] - 1) * xdelta_grad
     return lat, lon
 
-
+#%%
 def read_daily_regnie_ascii_grid(path_to_file):
     "read an ascii grid into a map, without the no-data values"
     with open(path_to_file) as file_:
         data = {}
-        row = 0
-        for line in file_:
-            if line[0] == " ":
-                continue
-            col = 0
-            for i in range(0, len(line)-1, 4):
-                val = int(line[i:i+4])
-                if val != -999:
-                    data[(row, col)] = val / 10.0
-                col += 1
-            row += 1
+        for y in range(971):
+            for x in range(611):
+                val = int(file_.read(4))
+                if val < 0:
+                    continue
+                data[(y, x)] = val / 10.0
+            file_.read(1)
         return data
 
 
-
+#%%
 def main():
 
+    #%%
     config = {
         "path_to_data": "m:/data/climate/dwd/grids/germany/daily/",
-        "path_to_output": "m:/data/climate/dwd/csvs/germany/",
-        "start": 1,
-        "end": 938
+        "path_to_output": "m:/data/climate/dwd/csvs/germany/daily-regnie/",
+        "start-year": 1995,
+        "end-year": 2012,
+        "start-month": 1,
+        "end-month": 12
     }
+
+    #%%
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
             kkk, vvv = arg.split("=")
             if kkk in config:
                 config[kkk] = int(vvv)
 
+    #%%
     path_to_data = config["path_to_data"]
     path_to_output = config["path_to_output"]
 
+    #%%
     ref_data = Dataset(path_to_data + "tavg_199501_daymean.nc")
     #no_of_days = len(ref_data.variables["time"])
 
+    #%%
     # first create a mapping of regnie cells to netcdf cells based on lat/lon proximity
     nc_to_regnie = {}
-    for f in os.listdir("out"):
-        with open("out/" + f) as _:
-            dic = pickle.load(_)
-            nc_to_regnie.update(dic)
+    with open("nc_to_regnie_1-938.pickle") as _:
+        nc_to_regnie = pickle.load(_)
 
-
+    #%%
     len_ys = len(ref_data.dimensions["y"])
     len_xs = len(ref_data.dimensions["x"])
 
-    # put all precip data of current month into dict for easy access like with netcdfs
-    regnie = {}
-    for day in range(no_of_days):
-        path_to_regniefile = path_to_data + "../daily-regnie/ra" + str(year) + "m/ra" + date(year, month, day+1).strftime("%y%m%d")
-        regnie[day] = read_daily_regnie_ascii_grid(path_to_regniefile)
+    for year in range(config["start-year"], config["end-year"]+1):
+        for month in range(config["start-month"], config["end-month"]+1):
 
-    for y in range(len_ys):
-        for x in range(len_xs):
+            days_in_month = calendar.monthrange(year, month)[1]
+            path_to_file = path_to_data + "../daily-precip3/precip_" + date(year, month, 1).strftime("%Y%m") + "_daymean.nc"
+            #path_to_file = "out/precip_" + date(year, month, 1).strftime("%Y%m") + "_daymean.nc"
 
-            lat = ref_data.variables["lat"][y, x]
-            lon = ref_data.variables["lon"][y, x]
-            reg_row, reg_col = find_closest_lat_lon(regnie_lat_lon, lat, lon)
-            reg_coord = (reg_row, reg_col) if reg_row and reg_col else None
+            daily = Dataset(path_to_file, "w")
 
-            if not reg_coord:
-                print "year: ", year, " month: ", " no regnie coord found for lat/lon: ", lat, "/", lon, " y/x: ", y, "/", x, " skipping"
-                continue
+            daily.createDimension("x", 720)
+            daily.createDimension("y", 938)
+            daily.createDimension("time", None)
 
-            path_to_outdir = path_to_output + "row-" + str(y) + "/"
-            if not os.path.isdir(path_to_outdir):
-                os.makedirs(path_to_outdir)
+            daily.createVariable("lon", "f8", ("y", "x"), fill_value=9999.0)
+            daily.variables["lon"][:, :] = ref_data.variables["lon"][:, :]
 
-            path_to_outfile = path_to_outdir + "col-" + str(x)
-            if not os.path.isfile(path_to_outfile):
-                with open(path_to_outfile, "w") as _:
-                    _.write(["iso-date", "tmin", "tavg", "tmax", "precip", "relhumid", "globrad", "windspeed"])
-                    _.write(["[]", "[°C]", "[°C]", "[°C]", "[mm]", "[%]", "[MJ m-2]", "[m s-1]"])
+            daily.createVariable("lat", "f8", ("y", "x"), fill_value=9999.0)
+            daily.variables["lat"][:, :] = ref_data.variables["lat"][:, :]
 
-            with open(path_to_outdir + "col-" + str(x), "a") as _:
-                writer = csv.writer(_, delimiter=",")
-                for i in range(no_of_days):
-                    row = [
-                        date(year, month, i+1).strftime("%Y-%m-%d"),
-                        str(data["tmin"].variables["temperature"][i][y][x]),
-                        str(data["tavg"].variables["temperature"][i][y][x]),
-                        str(data["tmax"].variables["temperature"][i][y][x]),
-                        str(regnie[i][reg_coord]) if reg_coord and reg_coord in regnie[i] else "0",
-                        str(data["RH"].variables["humidity"][i][y][x]),
-                        str(data["SIS"].variables["SIS"][i][y][x]),
-                        str(data["FF"].variables["FF"][i][y][x])
-                    ]
-                    writer.writerow(row)
-            
+            daily.createVariable("time", "i4", ("time"))
+            daily.variables["time"][:] = np.arange(days_in_month)
+
+            daily.createVariable("precipitation", "i2", ("time", "y", "x"), fill_value=9999)
+            daily.variables["precipitation"].scale_factor = 0.1
+
+            nc = daily.variables["precipitation"]
+
+            for day in range(days_in_month):
+                path_to_regniefile = path_to_data + "../daily-regnie/ra" + str(year) + "m/ra" + date(year, month, day+1).strftime("%y%m%d")
+                regnie_ascii = read_daily_regnie_ascii_grid(path_to_regniefile)
+
+                #path_to_file = "out/precip_" + date(year, month, day+1).strftime("%Y%m%d") + ".nc"
+                #daily = Dataset(path_to_file, "w")
+
+                #daily.createDimension("x", 720)
+                #daily.createDimension("y", 938)
+
+                #daily.createVariable("lon", "f8", ("y", "x"), fill_value=9999.0)
+                #daily.variables["lon"][:, :] = ref_data.variables["lon"][:, :]
+
+                #daily.createVariable("lat", "f8", ("y", "x"), fill_value=9999.0)
+                #daily.variables["lat"][:, :] = ref_data.variables["lat"][:, :]
+
+                #daily.createVariable("precipitation", "i2", ("y", "x"), fill_value=9999)
+                #daily.variables["precipitation"].scale_factor = 0.1
+
+                #nc_day = daily.variables["precipitation"]
+
+                for y in range(len_ys):
+                    for x in range(len_xs):
+
+                        #lat = ref_data.variables["lat"][y, x]
+                        #lon = ref_data.variables["lon"][y, x]
+                        reg_coord = nc_to_regnie[(y, x)]
+                        if not reg_coord or reg_coord not in regnie_ascii:
+                            nc[day, y, x] = 9999
+                            #print "nc(", y, "/", x, "): None"
+                            continue
+
+                        reg_val = regnie_ascii[(reg_coord)]
+                        nc[day, y, x] = reg_val
+                        #print "nc(", y, "/", x, "): ", reg_val
+
+                    print date(year, month, day+1), " nc.y: ", y, " done"
+
+                #daily.close()
+
+            daily.close()
+
     ref_data.close()
 
 main()
@@ -190,7 +222,7 @@ def main_write_nc_to_regnie():
                 return cur
             elif cur <= val and nex and val <= nex:
                 cur_dist = val - cur
-                nex_dist = nex - lat
+                nex_dist = nex - val
                 return cur if cur_dist <= nex_dist else nex
 
         return None
@@ -200,9 +232,10 @@ def main_write_nc_to_regnie():
         "find the (row, col) of the closest lat/lon value in dictionary regdic"
         closest_lat = find_closest_value(regdic, lat)
         if closest_lat:
-            closest_lon = find_closest_value(regdic[closest_lat], lon)
+            lons = regdic[closest_lat]
+            closest_lon = find_closest_value(lons, lon)
             if closest_lon:
-                return regdic[closest_lat][closest_lon]
+                return lons[closest_lon]
 
         return (None, None)
 
@@ -210,8 +243,14 @@ def main_write_nc_to_regnie():
     ref_data = Dataset(path_to_data + "tavg_199501_daymean.nc")
     #no_of_days = len(ref_data.variables["time"])
 
-    regnie_lat_lon = create_regnie_dicts()
-
+    regnie_lat_lon = OrderedDict()
+    regnie_lat_lon_unsorted = create_regnie_dicts()
+    for lat in sorted(regnie_lat_lon_unsorted.keys()):
+        regnie_lat_lon[lat] = OrderedDict()
+        ulons = regnie_lat_lon_unsorted[lat]
+        lons = regnie_lat_lon[lat]
+        for lon in sorted(ulons.keys()):
+            lons[lon] = regnie_lat_lon_unsorted[lat][lon]
 
     # first create a mapping of regnie cells to netcdf cells based on lat/lon proximity
     nc_to_regnie = {}
@@ -224,13 +263,11 @@ def main_write_nc_to_regnie():
 
             lat = ref_data.variables["lat"][y, x]
             lon = ref_data.variables["lon"][y, x]
-            reg_row, reg_col = find_closest_lat_lon(regnie_lat_lon, lat, lon)
-            reg_coord = (reg_row, reg_col) if reg_row and reg_col else None
+            reg_y, reg_x = find_closest_lat_lon(regnie_lat_lon, lat, lon)
+            nc_to_regnie[(y,x)] = (reg_y, reg_x) if reg_y and reg_x else None
 
-            nc_to_regnie[(y,x)] = reg_coord
-
-        yc = yc + 1
         print yc, " of ", (config["end"] - config["start"] + 1), " done"
+        yc = yc + 1
 
 
     with open("out/nc_to_regnie_" + str(config["start"]) + "-" + str(config["end"]) + ".pickle", "w") as _:
