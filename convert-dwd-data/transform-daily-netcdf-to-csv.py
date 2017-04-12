7e8c7215a19d72c96d7a3f1b2819b6e8675956ca
Michael Berg-Mohnicke <michael.berg@zalf.de>
2017-04-12 15:22:50 +0200

    final version

--- convert-dwd-data/transform-daily-netcdf-to-csv.py
+++ convert-dwd-data/transform-daily-netcdf-to-csv.py
@@ -34,15 +34,20 @@ print sys.path
 from netCDF4 import Dataset
 import numpy as np
 
-LOCAL_RUN = False
+LOCAL_RUN = True
 
 def main():
 
     config = {
         "path_to_data": "m:/data/climate/dwd/grids/germany/daily/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/grids/germany/daily/",
-        "path_to_output": "m:/data/climate/dwd/csvs/germany/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/csvs/germany/",
-        "from-y": 1,
-        "to-y": -1
+        #"path_to_output": "m:/data/climate/dwd/csvs/germany/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/csvs/germany/",
+        "path_to_output": "g:/csvs/germany/" if LOCAL_RUN else "/archiv-daten/md/data/climate/dwd/csvs/germany/",
+        "start-y": 1,
+        "end-y": -1,
+        "start-year": 1995,
+        "start-month": 1,
+        "end-year": 2012,
+        "end-month": 12
     }
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
@@ -57,35 +62,78 @@ def main():
         month = int(year_month[4:])
         files[year][month][elem] = config["path_to_data"] + f
 
+    elem_to_varname = {
+        "tmin": "temperature",
+        "tavg": "temperature",
+        "tmax": "temperature",
+        "precip": "precipitation",
+        "RH": "humidity",
+        "SIS": "SIS",
+        "FF": "FF"
+    }
+
+
+    def write_files(cache):
+        no_of_files = len(cache)
+        count = 0
+        for (y, x), rows in cache.iteritems():
+            path_to_outdir = config["path_to_output"] + "row-" + str(y) + "/"
+            if not os.path.isdir(path_to_outdir):
+                os.makedirs(path_to_outdir)
+
+            path_to_outfile = path_to_outdir + "col-" + str(x) + ".csv"
+            if not os.path.isfile(path_to_outfile):
+                with open(path_to_outfile, "wb") as _:
+                    writer = csv.writer(_, delimiter=",")
+                    writer.writerow(["iso-date", "tmin", "tavg", "tmax", "precip", "relhumid", "globrad", "windspeed"])
+                    writer.writerow(["[]", "[°C]", "[°C]", "[°C]", "[mm]", "[%]", "[MJ m-2]", "[m s-1]"])
+
+            with open(path_to_outfile, "ab") as _:
+                writer = csv.writer(_, delimiter=",")
+                for row in rows:
+                    writer.writerow(row)
+
+            count = count + 1
+            if count % 1000 == 0:
+                print count, "/", no_of_files, "written"
+
+
+    write_files_threshold = 50 #ys
     for year, months in files.iteritems():
+        if year < config["start-year"]:
+            continue
+        if year > config["end-year"]:
+            break
+
         #print "year: ", year, "months: ",
         for month, elems in months.iteritems():
-            print "year: ", year, " month: ", month, " ys -> ",
+            if year == config["start-year"] and month < config["start-month"]:
+                continue
+            if year == config["end-year"] and month > config["end-month"]:
+                break
+
+            print "year:", year, "month:", month, "ys ->",
             data = {}
             for elem, filepath in elems.iteritems():
-                data[elem] = Dataset(filepath).variables[{
-                    "tmin": "temperature",
-                    "tavg": "temperature",
-                    "tmax": "temperature",
-                    "precip": "precipitation",
-                    "RH": "humidity",
-                    "SIS": "SIS",
-                    "FF": "FF"}[elem]].copy()
+                ds = Dataset(filepath)
+                data[elem] = np.copy(ds.variables[elem_to_varname[elem]])
+                ds.close()
 
             ref_data = data["tavg"]
-            no_of_days = len(ref_data.variables["time"])
+            #no_of_days = len(ref_data.variables["time"])
+            no_of_days = ref_data.shape[0]
 
             start_month = time.clock()
             cache = defaultdict(list)
 
-            for y in range(config["from-y"] - 1, len(ref_data.dimensions["y"]) if config["to-y"] < 0 else config["to-y"]):
+            for y in range(config["start-y"] - 1, ref_data.shape[1] if config["end-y"] < 0 else config["end-y"]):
                 #print "y: ", y, "->"
                 start_y = time.clock()
                 #print y,
-                for x in range(len(ref_data.dimensions["x"])):
+                for x in range(ref_data.shape[2]):
                     #print x,
 
-                    if ref_data.variables["temperature"][0, y, x] is np.ma.masked:
+                    if int(ref_data[0, y, x]) == 9999:
                         continue
 
                     #lat = ref_data.variables["lat"][y, x]
@@ -113,34 +161,24 @@ def main():
                         cache[(y,x)].append(row)
                 
                 end_y = time.clock()
-                print str(y) + "|" + str(int(end_y - start_y)) + "s ",
-                #print ""
+                print y, #str(y) + "|" + str(int(end_y - start_y)) + "s ",
+               
+                if y > config["start-y"] and y % write_files_threshold == 0:
+                    print ""
+                    s = time.clock()
+                    write_files(cache)
+                    cache = defaultdict(list)
+                    e = time.clock()
+                    print "wrote", write_files_threshold, "ys in", (e-s), "seconds"
 
-            for dataset in data.values():
-                dataset.close()
+            #for dataset in data.values():
+            #    dataset.close()
             print ""
 
-            end_filled_cache = time.clock()
-            print "filling cache took ", (end_filled_cache - start_month), " seconds"
+            #write remaining cache items
+            write_files(cache)
 
-            for (y, x), rows in cache.iteritems():
-                path_to_outdir = config["path_to_output"] + "row-" + str(y) + "/"
-                if not os.path.isdir(path_to_outdir):
-                    os.makedirs(path_to_outdir)
-
-                path_to_outfile = path_to_outdir + "col-" + str(x) + ".csv"
-                if not os.path.isfile(path_to_outfile):
-                    with open(path_to_outfile, "wb") as _:
-                        writer = csv.writer(_, delimiter=",")
-                        writer.writerow(["iso-date", "tmin", "tavg", "tmax", "precip", "relhumid", "globrad", "windspeed"])
-                        writer.writerow(["[]", "[°C]", "[°C]", "[°C]", "[mm]", "[%]", "[MJ m-2]", "[m s-1]"])
-
-                with open(path_to_outfile, "ab") as _:
-                    writer = csv.writer(_, delimiter=",")
-                    for row in rows:
-                        writer.writerow(row)
-
-            end_wrote_files = time.clock()
-            print "writing files took ", (end_wrote_files - end_filled_cache), " seconds"
+            end_month = time.clock()
+            print "running month", month, "took", (end_month - start_month), "seconds"
 
 main()
